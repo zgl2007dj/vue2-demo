fb23c7da9dde2b8376e8bd184ced262a5c01f13a
Rak <zgl2008dj@gmail.com>
2017-06-26 11:08:48 +0800

    自动挂载当前目录的组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,11 @@
-import header from './header'
-export default { header }
+const components = {}
+
+let req = require.context('./', true, /^(.*\.(vue$))[^.]*$/igm);
+req.keys().forEach(function(key) {
+  let _key = key.replace('./', '').replace('.vue', '')
+  // 文件夹处理
+  _key = _key.replace(/\//g, '-')
+  _key && components[_key] = req(key);
+});
+
+module.exports = components
